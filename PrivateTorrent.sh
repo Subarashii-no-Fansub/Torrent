dc9bafe4ff07d33901983c3f5b1f2c013fa752f9
Subarashii no Fansub <subarashii-no-fansub@null.net>
2014-07-11 19:57:09 +0200

    corrige la variable

--- PrivateTorrent.sh
+++ PrivateTorrent.sh
@@ -10,12 +10,12 @@ then
   echo -n "Taille du dossier : " && du -hs "$1"
   echo -n "Nombre de fichiers à l'intérieur : " && find "$1" -maxdepth 1 -type f | wc -l
 else
-  echo -n "Taille du fichier en bytes :" && stat --format="%s" "$1"
+  echo -n "Taille du fichier en bytes : " && stat --format="%s" "$1"
 fi
 
 echo "     = = = = =     "
 
-transmission-create -o "$setdirectory/$file.torrent" --private --tracker http://tracker.t411.me:56969/ "$1"
+transmission-create -o "$setdirectory/$1.torrent" --private --tracker http://tracker.t411.me:56969/ "$1"
 chmod 644 "$setdirectory/$file.torrent"
 
 if [ -d "$1" ]
