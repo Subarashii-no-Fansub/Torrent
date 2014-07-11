a72b5e93420abc667f866cf4f7066fabc5a0dbb4
Subarashii no Fansub <subarashii-no-fansub@null.net>
2014-07-11 19:56:11 +0200

    problème d'espace

--- PrivateTorrent.sh
+++ PrivateTorrent.sh
@@ -5,12 +5,12 @@ set -e
 setdirectory='/home/ddl/public_html'
 
 echo "Informations complémentaires"
-if [ -d $1 ]
+if [ -d "$1" ]
 then
-  echo -n "Taille du dossier : " && du -hs $1
-  echo -n "Nombre de fichiers à l'intérieur : " && find $1 -maxdepth 1 -type f | wc -l
+  echo -n "Taille du dossier : " && du -hs "$1"
+  echo -n "Nombre de fichiers à l'intérieur : " && find "$1" -maxdepth 1 -type f | wc -l
 else
-  echo -n "Taille du fichier en bytes :" && stat --format="%s" $1
+  echo -n "Taille du fichier en bytes :" && stat --format="%s" "$1"
 fi
 
 echo "     = = = = =     "
@@ -18,15 +18,15 @@ echo "     = = = = =     "
 transmission-create -o "$setdirectory/$file.torrent" --private --tracker http://tracker.t411.me:56969/ "$1"
 chmod 644 "$setdirectory/$file.torrent"
 
-if [ -d $1 ]
+if [ -d "$1" ]
 then
-  filename=$(basename $1)
+  filename=$(basename "$1")
 else
-  fullfilename=$(basename $1)
+  fullfilename=$(basename "$1")
   filename=${fullfilename%.*}
 fi
 
-mediainfo $1 > "$setdirectory/$filename".nfo
+mediainfo "$1" > "$setdirectory/$filename".nfo
 chmod 644 "$setdirectory/$filename".nfo
 
 echo "FINI !"
